06c658a604ce28af7f59d5b4d32481c3eeb1c4fa
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-08-14 16:09:41 -0400

    feat: add Shipments table to index

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 const UserRating = require('./user-rating');
+const Shipments = require('./shipments');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -16,5 +17,6 @@ const UserRating = require('./user-rating');
  */
 module.exports = {
   User,
-  UserRating
+  UserRating,
+  Shipments
 }
