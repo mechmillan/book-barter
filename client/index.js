f8d1820ff32b630b551e9ded735af2ac54efbe7f
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-11-03 16:50:31 -0400

    minor cleanup

--- client/index.js
+++ client/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import './index.css';
-
-// import store and Provider
 import { Provider } from "react-redux";
-import store from "./store"; // assumes index.js
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
