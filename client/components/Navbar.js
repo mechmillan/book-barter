7bc2c4ce3460f692d587421f91faaed91f6e8114
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-11-03 16:59:43 -0400

    feat: test out styled-components

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,17 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  display: flex;
+  background: dodgerblue;
+`;
+
 const Navbar = () => {
   return (
     <Router>
-      <div className="navigation">
-        <nav>
+      <div>
+        <StyledNav>
           <Link to="/home">BookBarter</Link>
           <Link to="/my-books">My Books</Link>
           <Link to="/looking-for">Looking For</Link>
           <Link to="/login">Login</Link>
           <Link to="/logout">Logout</Link>
-        </nav>
+        </StyledNav>
         <Route exact path="/home" component={Home} />
       </div>
     </Router>
