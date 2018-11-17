00ebc414834411824258258705f0045c5e267ced
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-11-17 12:35:05 -0500

    style ButtonLink in Navbar

--- client/components/Navbar/Navbar.css.js
+++ client/components/Navbar/Navbar.css.js
@@ -1,14 +1,35 @@
-import styled, { Link } from "styled-components";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Nav = styled.nav`
   display: flex;
   border: 1px solid black;
   flex-direction: row;
+  justify-content: space-evenly;
+  height: 50px;
 `;
 
 export const ButtonLink = styled(Link)`
-  border-radius: 2px;
-  border: 1px solid dodgerblue;
-  margin: 10px;
-  padding: 5px;
+  align-items: center;
+  background-color: none;
+  border-radius: 3px;
+  border: 1px solid grey;
+  color: grey;
+  display: flex;
+  font-size: 12px;
+  font-weight: 200;
+  height: 25px;
+  justify-content: center;
+  margin: 10px 5px;
+  padding: 0 10px;
+
+  &:hover {
+    background-color: dodgerblue;
+    cursor: pointer;
+  }
+
+  &:disabled {
+    background-color: grey;
+    cursor: default;
+  }
 `;
---
+++ client/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../Home/Home";
+
+import { ButtonLink, Nav } from "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <Router>
+      <>
+        <Nav>
+          <ButtonLink to="/home">BookBarter</ButtonLink>
+          <ButtonLink to="/my-books">My Books</ButtonLink>
+          <ButtonLink to="/looking-for">Looking For</ButtonLink>
+          <ButtonLink to="/login">Login</ButtonLink>
+          <ButtonLink to="/logout">Logout</ButtonLink>
+        </Nav>
+
+        <Route exact path="/home" component={Home} />
+      </>
+    </Router>
+  );
+};
+
+export default Navbar;
