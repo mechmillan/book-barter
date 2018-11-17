a35c7db0897034edbd319ba56a6547a8fff97686
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-11-17 12:27:04 -0500

    update entry point

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: isDev ? "development" : "production",
   entry: [
     "@babel/polyfill", // enables async-await
-    "./client/index.js"
+    "./client/components/App/index.js"
   ],
   output: {
     path: __dirname,
