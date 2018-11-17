12f0a09efa741eb40ac75c34b73fc330dcd1214d
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-11-17 12:40:51 -0500

    add sample template

---
+++ client/components/_template/_template.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import { Wrapper } from "./_template.css";
+
+class Template extends Component {
+  render() {
+    return <Wrapper>Template page</Wrapper>;
+  }
+}
+
+export default Template;
